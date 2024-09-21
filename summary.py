93be62c1c8d07d48c93c52c0e50c2976a4a0b357
Nwosu David <snjhauser@gmail.com>
2024-09-21 22:38:35 +0100

    @feat: summary by month

--- summary.py
+++ summary.py
@@ -5,4 +5,5 @@ def setup_summary_parser(subparsers):
     Setup for the 'summary' subcommand
     """
     summary_parser = subparsers.add_parser('summary', help='Summarize all expenses')
+    add_parser.add_argument('--month', type=int, required=False, help='Specific month (of current year)')
     return summary_parser
